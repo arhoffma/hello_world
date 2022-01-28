6e29b801fbe23d2289d956240ebc02c74811a2c0
Andrew <andrew.r.hoffman@gmail.com>
2022-01-27 21:13:46 -0600

    updated/edited line 34 through line 45

--- RecursivePalindrome.java
+++ RecursivePalindrome.java
@@ -7,42 +7,42 @@ This program decides whether a string is a palindrome using recursion
 public class RecursivePalindrome {
 
 	// method isPalindrome 
-	public static boolean isPalindrome(String s) {
+	public static boolean isPalindrome(String str) {
 
-		return isPalindrome(s, 0, s.length()-1);
+		return isPalindrome(str, 0, str.length()-1);
 
 	}
 	
 	
 	// helper method isPalindrome
-	private static boolean isPalindrome(String s, int low, int high) {
+	private static boolean isPalindrome(String str, int low, int high) {
 	
 		if (high<=low) // Base case
 			return true;
-		else if (s.charAt(low) != s.charAt(high)) // Base case
+		else if (str.charAt(low) != str.charAt(high)) // Base case
 			return false;
 		else
-			return isPalindrome(s, low + 1, high -1); // Recursive call
+			return isPalindrome(str, low + 1, high -1); // Recursive call
 				
 	}
 	
-	// main method with Scanner object
+	// main method with scanner object
 	public static void main(String[] args) {
 	
 		Scanner input = new Scanner(System.in);
 		System.out.print("Enter a string: "); // ptompt for user input
 		
-		String s = input.nextLine(); // assign user input to variable s
+		String str = input.nextLine(); // assign user input to variable s
 		
 		// print output to console
-		System.out.println("Is " + s + " a palindrome? " + isPalindrome(s));	
+		System.out.println("Is " + str + " a palindrome? " + isPalindrome(str));	
 	
 		/*
-		System.out.println("Is moon an palindrome? " + isPalindrome("moon"));
-		System.out.println("Is noon an palindrome? " + isPalindrome("noon"));
-		System.out.println("Is a a palindrome? " + isPalindrome("a"));
-		System.out.println("Is aba a palindrome? " + isPalindrome("aba"));
-		System.out.println("Is ab a palindrome? " + isPalindrome("ab"));
+		system.out.println("Is moon an palindrome? " + isPalindrome("moon"));
+		system.out.println("Is noon an palindrome? " + isPalindrome("noon"));
+		system.out.println("Is a a palindrome? " + isPalindrome("a"));
+		system.out.println("Is aba a palindrome? " + isPalindrome("aba"));
+		system.out.println("Is ab a palindrome? " + isPalindrome("ab"));
 		*/
 		
 		input.close(); // close input 
