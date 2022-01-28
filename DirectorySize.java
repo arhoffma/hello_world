f01776fdadafb61005a8f49288ee69e1cb9078eb
Andrew <andrew.r.hoffman@gmail.com>
2022-01-27 21:33:44 -0600

    added DirectorySize.java

---
+++ DirectorySize.java
@@ -0,0 +1,42 @@
+/**
+This will prompt a user to enter a directory and display its size.
+**/
+
+import java.io.File;
+import java.util.Scanner;
+
+public class DirectorySize {
+
+
+	// method getSize 
+	public static long getSize(File file) {
+	
+		long size = 0; // store total size of all files
+		
+		if (file.isDirectory()) {
+			File[] files = file.listFiles(); // all files and subdirectories
+			for (int i = 0; files != null && i < files.length; i++) {
+				size += getSize(files[i]); // recursive call	
+			}
+		}
+		else{
+			size += file.length(); // base case
+		}
+		return size;
+	}
+	
+	// main method
+	public static void main(String[] args) {
+		
+		// prompt for user input	
+		System.out.print("Enter a directory or file: ");
+		
+		// new Scanner object and assign user input to String directory
+		String directory = new Scanner(System.in).nextLine();
+		
+		// print output to console
+		System.out.println(getSize(new File(directory)) + " bytes");
+	}
+
+
+}
